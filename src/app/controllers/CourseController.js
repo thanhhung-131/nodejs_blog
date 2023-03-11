5fc623cf6e25e20da1bc1accee1bdf8920b6a9bb
Daiki <hothanhhung131@gmail.com>
2023-03-11 08:10:08 +0700

    change folder level

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,14 +1,26 @@
-const Course = require('./models/Course')
-const { multipleMongooseToObject } = require('../../util/mongoose')
+const Course = require('../models/Course')
+const { mongooseToObject } = require('../../util/mongoose')
 
 class CourseController {
   // [GET] /courses/:slug
   show(req, res, next) {
     Course.findOne({slug: req.params.slug})
       .then((course) => {
-        res.render('courses/show.hbs')
+        res.render('courses/show', { course: mongooseToObject(course) })
       })
       .catch(next);
+  }
+
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render('courses/create')
+  }
+
+  // [POST] /courses/store
+  store(req, res, next) {
+    // res.json(req.body)
+    const course = new Course(req.body);
+    course.save()
   }
 }
 
